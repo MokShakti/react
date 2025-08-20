591575a7adfe232295049c3a895405168cd435a3
MokShakti <mokshakti7@gmail.com>
2025-08-20 23:50:49 +0200

    lesson_07

--- lesson_07/src/api/apiRoutes.js
+++ lesson_07/src/api/apiRoutes.js
@@ -1,9 +1,18 @@
-const BASE = import.meta.env.VITE_API_URL || ''
+// const BASE = import.meta.env.VITE_API_URL || ''
 
-export const API = {
-   products: {
-      list: () => `${BASE}/api/products`,
-      byId: (id) => `${BASE}/api/products/${id}`,
-      search: (q) => `${BASE}/api/products/search?q=${encodeURIComponent(q)}`,
-   },
+// export const API = {
+//    products: {
+//       list: () => `${BASE}/api/products`,
+//       byId: (id) => `${BASE}/api/products/${id}`,
+//       search: (q) => `${BASE}/api/products/search?q=${encodeURIComponent(q)}`,
+//    },
+// }
+
+
+const API_BASE = 'https://backend-2-tqj2.onrender.com/api';
+
+export default {
+   productsList: `${API_BASE}/products`,
+   getProductByCategory: (category) => `${API_BASE} /products/category / ${category}`,
+   getProductById: (id) => `${API_BASE} /products/${id}`
 }
